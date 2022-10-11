13c7d7fad82055015be77da0e90aa74255b7b8af
student <student@info-11.cshl.edu>
2022-10-11 10:28:26 -0400

    added command line arguments

--- irene.py
+++ irene.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+import sys
 
-myname = 'Irene'
-print('My name: ' + myname)
+myname = sys.argv[1]
+favcolor = sys.argv[2]
+favactivity = sys.argv[3]
+favanimal = sys.argv[4]
+
+print('My name:', myname)
+print('My favorite color:', favcolor)
+print('My favorite activity:', favactivity)
+print('My favorite animal:', favanimal)
