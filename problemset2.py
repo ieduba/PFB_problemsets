1d66ce7f4eb1be5117ade716d78f6ab4a60e6906
student <student@info-11.cshl.edu>
2022-10-11 15:47:31 -0400

    added more tests

--- problemset2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-var = False
-
-if bool(var) == True:
-	print('value is true')
-else:
-	print('value is not true')
--- py2q3.py
+++ py2q3.py
@@ -4,9 +4,21 @@ import sys
 num = sys.argv[1]
 
 num = int(num)
+print('num:',num)
 
 if num > 0:
-	print('positive')
+	if num < 50:
+		if num%2 == 0:
+			print('even number smaller than 50')
+		else:
+			print('odd number smaller than 50')
+	elif num > 50:
+		if num%3 == 0:
+			print('larger than 50, divisible by 3')
+		else:
+			print('larger than 50, not divisible by 3')
+	else:
+		print('fifty!')
 elif num < 0:
 	print('negative')
 else:
