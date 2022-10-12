d1c671a0b3c27d109af7cfeed3e7e90bc612987f
student <student@info-11.cshl.edu>
2022-10-12 16:21:19 -0400

    question 2 of python 4

---
+++ py4q2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+
+taxa = 'sapiens, erectus, neanderthalensis'
+print(taxa[1])
+print(type(taxa))
+
+species = taxa.split(', ')
+print(species)
+print(species[1])
+print(type(species))
+
+print(sorted(species))
+print(sorted(species,key=len))
