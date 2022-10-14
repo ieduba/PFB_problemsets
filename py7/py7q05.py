72deca8bdbf09b91b80fc2ef0fd722eb2b09c0c0
student <student@info-11.cshl.edu>
2022-10-14 12:00:15 -0400

    tweaked fasta header regex

--- py7/py7q05.py
+++ py7/py7q05.py
@@ -3,7 +3,7 @@ import re
 
 with open('Python_07.fasta','r') as fa:
 	fasta = fa.read()
-	for gene in re.finditer('>(\S+)\s*(.*)\n([ATCG\n]+)',fasta):
+	for gene in re.finditer('>(\S*)\s*(.*)\n([ATCG\n]+)',fasta):
 		geneid = gene.group(1)
 		description = gene.group(2)
 		sequence = gene.group(3)
