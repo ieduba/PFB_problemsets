cd062333729fea733111b2e0ff4a851fe978ef8c
student <student@info-11.cshl.edu>
2022-10-14 12:45:36 -0400

    added codon translation

--- py8/py8q02.py
+++ py8/py8q02.py
@@ -4,7 +4,31 @@ import re
 
 infile = sys.argv[1]
 
-with open(infile,'r') as file, open('Python_08.codons-6frames.nt','w') as writefile:
+translation_table = {
+    'GCT':'A', 'GCC':'A', 'GCA':'A', 'GCG':'A',
+    'CGT':'R', 'CGC':'R', 'CGA':'R', 'CGG':'R', 'AGA':'R', 'AGG':'R',
+    'AAT':'N', 'AAC':'N',
+    'GAT':'D', 'GAC':'D',
+    'TGT':'C', 'TGC':'C',
+    'CAA':'Q', 'CAG':'Q',
+    'GAA':'E', 'GAG':'E',
+    'GGT':'G', 'GGC':'G', 'GGA':'G', 'GGG':'G',
+    'CAT':'H', 'CAC':'H',
+    'ATT':'I', 'ATC':'I', 'ATA':'I',
+    'TTA':'L', 'TTG':'L', 'CTT':'L', 'CTC':'L', 'CTA':'L', 'CTG':'L',
+    'AAA':'K', 'AAG':'K',
+    'ATG':'M',
+    'TTT':'F', 'TTC':'F',
+    'CCT':'P', 'CCC':'P', 'CCA':'P', 'CCG':'P',
+    'TCT':'S', 'TCC':'S', 'TCA':'S', 'TCG':'S', 'AGT':'S', 'AGC':'S',
+    'ACT':'T', 'ACC':'T', 'ACA':'T', 'ACG':'T',
+    'TGG':'W',
+    'TAT':'Y', 'TAC':'Y',
+    'GTT':'V', 'GTC':'V', 'GTA':'V', 'GTG':'V',
+    'TAA':'*', 'TGA':'*', 'TAG':'*'
+}
+
+with open(infile,'r') as file, open('Python_08.codons-6frames.nt','w') as writecodons, open('Python_08.translated.aa','w') as writeaas:
 	fasta = file.read()
 	
 	seqs = {}
@@ -12,17 +36,20 @@ with open(infile,'r') as file, open('Python_08.codons-6frames.nt','w') as writef
 		seqs[gene.group(1)] = {}
 		sequence = gene.group(3).replace('\n','')
 		comp = sequence.replace('A','t').replace('T','a').replace('C','g').replace('G','c').upper()
-		rc = comp[::-1]	
-		seqs[gene.group(1)]['frame1'] = re.findall('([ATCG]{3})', sequence)
-		seqs[gene.group(1)]['frame2'] = re.findall('([ATCG]{3})', sequence[1:])
-		seqs[gene.group(1)]['frame3'] = re.findall('([ATCG]{3})', sequence[2:])
-		seqs[gene.group(1)]['frame1rc'] = re.findall('([ATCG]{3})', rc)
-		seqs[gene.group(1)]['frame2rc'] = re.findall('([ATCG]{3})', rc[1:])
-		seqs[gene.group(1)]['frame3rc'] = re.findall('([ATCG]{3})', rc[2:])
-	for key in seqs:
-		writefile.write(f'{key}-frame1-codons\n{seqs[key]["frame1"]}\n')
-		writefile.write(f'{key}-frame2-codons\n{seqs[key]["frame2"]}\n')
-		writefile.write(f'{key}-frame3-codons\n{seqs[key]["frame3"]}\n')
-		writefile.write(f'{key}-frame1rc-codons\n{seqs[key]["frame1rc"]}\n')
-		writefile.write(f'{key}-frame2rc-codons\n{seqs[key]["frame2rc"]}\n')
-		writefile.write(f'{key}-frame3rc-codons\n{seqs[key]["frame3rc"]}\n')
+		rc = comp[::-1]
+		for i in range(3):	
+			seqs[gene.group(1)][f'frame{i+1}'] = re.findall('([ATCG]{3})', sequence[i:])
+			seqs[gene.group(1)][f'frame{i+1}rc'] = re.findall('([ATCG]{3})', rc[i:])
+
+	aas = {}
+	for gene in seqs:
+		aas[gene] = {}
+		for frame in seqs[gene]:
+			aas[gene][frame] = []
+			for codon in seqs[gene][frame]:
+				aas[gene][frame].append(translation_table[codon])
+	
+	for gene in seqs:
+		for frame in seqs[gene]:
+			writecodons.write(f'{gene}-{frame}-codons\n{seqs[gene][frame]}\n')
+			writeaas.write(f'{gene}-{frame}-AAs\n{aas[gene][frame]}\n')
